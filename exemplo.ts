a1b1b3b6a02e9aa530d1e6350e07237634896f73
juniorbueno1988 <juniobueno@gmail.com>
2023-12-11 19:22:30 -0300

    Adiciona arquivo exemplo.ts e tsconfig.json

---
+++ exemplo.ts
@@ -0,0 +1,7 @@
+// exemplo.ts
+function saudacao(nome: string) {
+    return `Olá, ${nome}! Bem-vindo ao mundo do TypeScript.`;
+}
+
+const nomeUsuario = "SeuNome";
+console.log(saudacao(nomeUsuario));
